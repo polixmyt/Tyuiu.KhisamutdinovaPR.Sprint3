770b2c22a31e3cfde5cc9e7534394a7610d91a96
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-10-23 18:54:46 +0500

    Task1

--- Tyuiu.KhisamutdinovaPR.Sprint3.Task1.V3.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint3.Task1.V3.Lib/DataService.cs
@@ -1,26 +1,24 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
-namespace Tyuiu.KhisamutdinovaPR.Sprint3.Task1.V3.Lib;
+using System;
 
-public class DataService: ISprint3Task1V3
+namespace Tyuiu.KhisamutdinovaPR.Sprint3.Task1.V3.Lib
 {
-    // p = Π_{k=1..10} ( k / (cos(5) + 1)^2 )
-    public double Calculate()
+    public class DataService
     {
-        double p = 1.0;
-        int k = 1;
-        double den = Math.Pow(Math.Cos(5) + 1, 2);
-
-        while (k <= 10)
+        // p = Π_{k=1..10} ( k / (cos(5) + 1)^2 )
+        public double Calculate()
         {
-            p *= k / den;
-            k++;
+            double p = 1.0;
+            double den = Math.Pow(Math.Cos(5) + 1.0, 2.0); // важно: (cos(5)+1)^2
+            int k = 1;
+
+            while (k <= 10)
+            {
+                p *= k / den;   // k/int → double, т.к. den — double
+                k++;
+            }
+
+            return Math.Round(p, 3); // 24589.408
         }
-
-        return p;
-    }
-
-    public double GetMultiplySeries(int startValue, int stopValue)
-    {
-        throw new NotImplementedException();
     }
 }
