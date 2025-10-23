3107a9d9d14a96a788a46a4c22c265e708ce3b3c
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-10-23 13:38:07 +0500

    Task0

--- Tyuiu.KhisamutdinovaPR.Sprint3.Task0.V15.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint3.Task0.V15.Lib/DataService.cs
@@ -9,7 +9,7 @@ public class DataService : ISprint3Task0V15
         double summ = 0;
         for (int i = startValue; i <= stopValue; i++)
         {
-            summ += (Math.Pow(value, i) + 2/(i + 1)) * Math.Sin(value);
+            summ += (Math.Pow(value, i) + 2.0/(i + 1)) * Math.Sin(value);
         }
         return Math.Round(summ, 3);
     }
