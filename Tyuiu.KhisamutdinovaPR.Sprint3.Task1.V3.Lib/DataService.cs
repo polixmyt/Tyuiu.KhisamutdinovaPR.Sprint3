f32d75e5e0a85c30076f35789c89f7a492ebae31
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-10-23 18:50:03 +0500

    Task1

--- Tyuiu.KhisamutdinovaPR.Sprint3.Task1.V3.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint3.Task1.V3.Lib/DataService.cs
@@ -1,7 +1,7 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.KhisamutdinovaPR.Sprint3.Task1.V3.Lib;
 
-public class DataService
+public class DataService: ISprint3Task1V3
 {
     // p = Π_{k=1..10} ( k / (cos(5) + 1)^2 )
     public double Calculate()
@@ -17,5 +17,10 @@ public class DataService
         }
 
         return p;
+    }
+
+    public double GetMultiplySeries(int startValue, int stopValue)
+    {
+        throw new NotImplementedException();
     }
 }
