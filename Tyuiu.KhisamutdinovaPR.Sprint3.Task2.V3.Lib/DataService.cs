ea1d79edaab7c0c67b77df876fccd1480ac7cd1f
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-10-27 20:32:18 +0500

    Task2

--- Tyuiu.KhisamutdinovaPR.Sprint3.Task2.V3.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint3.Task2.V3.Lib/DataService.cs
@@ -1,26 +1,25 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.KhisamutdinovaPR.Sprint3.Task2.V3.Lib;
+
+public class DataService : ISprint3Task2V3
 {
-    public class DataService: ISprint3Task2V3
+    public double CalculateSeriesSum(int x)
     {
-        public double CalculateSeriesSum(int x)
+        double s = 0;
+        int i = 1;
+
+        do
         {
-            double s = 0;
-            int i = 1;
-
-            do
-            {
-                s += (Math.Pow(x, 2) * i) + 1;
-                i++;
-            }
-            while (i <= 15);
-
-            return s;
+            s += (Math.Pow(x, 2) * i) + 1;
+            i++;
         }
+        while (i <= 15);
+
+        return s;
+    }
 
     public double GetSumSeries(int value, int startValue, int stopValue)
     {
         throw new NotImplementedException();
     }
-}
 }
