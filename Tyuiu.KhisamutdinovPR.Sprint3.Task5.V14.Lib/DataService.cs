ba782f25acfcd448cb771054870f1a8bd79ddb4c
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-10-29 19:19:34 +0500

    Task5

--- Tyuiu.KhisamutdinovPR.Sprint3.Task5.V14.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovPR.Sprint3.Task5.V14.Lib/DataService.cs
@@ -2,7 +2,7 @@
 using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.KhisamutdinovPR.Sprint3.Task5.V14.Lib
 {
-    public class DataService: ISprint3Task5V14
+    public class DataService: ISprint3Task5V14 
     {
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
